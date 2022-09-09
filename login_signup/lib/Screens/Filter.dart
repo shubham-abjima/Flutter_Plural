bbbab99720a8b4ba5f348a5352dbdcb42fbebb3a
Shubham Khandelwal <shubham.khandelwal@abjima.com>
2022-09-09 18:42:16 +0530

    CVR FILTER

--- login_signup/lib/Screens/Filter.dart
+++ login_signup/lib/Screens/Filter.dart
@@ -1,4 +1,9 @@
+import 'dart:ffi';
+import 'dart:ui';
+
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:login_with_signup/Comm/genTextFormField.dart';
 import 'package:login_with_signup/Screens/HomeForm.dart';
 import 'package:login_with_signup/Screens/HomePage.dart';
 
@@ -9,25 +14,307 @@ class Filter extends StatefulWidget {
   State<Filter> createState() => _FilterState();
 }
 
+List<String> localData = [
+  'One',
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  'Six',
+  'Seven',
+  'Eight',
+  'Nine',
+  'Ten',
+];
+
 class _FilterState extends State<Filter> {
+  Map<String, String> selectedValueMap = Map();
+
   @override
+  void initState() {
+    selectedValueMap["local"] = null;
+    selectedValueMap["server"] = null;
+    super.initState();
+  }
+
+  @override
+  int selectedValue = 1;
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        automaticallyImplyLeading: true,
-        title: Text(
-          'FILTER APPOINTMENT',
-          style: TextStyle(color: Colors.black),
+        appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          title: Text(
+            'FILTER APPOINTMENT',
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (_) => MyHomePage()),
+                (Route<dynamic> route) => false),
+          ),
         ),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (_) => MyHomePage()),
-              (Route<dynamic> route) => false),
-        ),
-      ),
-    );
+        body: SingleChildScrollView(
+            child: InkWell(
+          child: Container(
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 20, 20, 0),
+                    child: Text(
+                      "SEARCH BY LOCATION",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+
+              // DropdownSearch(
+              //   items: ["Brazil", "France", "Tunisia", "Canada"],
+              //   onChanged: print,
+              //   selectedItem: "Tunisia",
+              //   validator: (String item) {
+              //     if (item == null)
+              //       return "Required field";
+              //     else if (item == "Brazil")
+              //       return "Invalid item";
+              //     else
+              //       return null;
+              //   },
+              // ),
+              Container(),
+              Container(
+                child: Text("Hello"),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 20, 20, 0),
+                    child: Text(
+                      "DATE RANGE",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton(
+                      color: Color.fromARGB(255, 225, 223, 223),
+                      minWidth: 5,
+                      onPressed: () => {},
+                      child: Text(
+                        "TODAY",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      splashColor: Colors.redAccent,
+                    ),
+                    FlatButton(
+                      color: Color.fromARGB(255, 225, 223, 223),
+                      minWidth: 5,
+                      onPressed: () {},
+                      child: Text(
+                        "7 DAYS",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      splashColor: Colors.redAccent,
+                    ),
+                    FlatButton(
+                      color: Color.fromARGB(255, 225, 223, 223),
+                      minWidth: 5,
+                      onPressed: () {},
+                      child: Text(
+                        "LAST MONTH",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      splashColor: Colors.redAccent,
+                    ),
+                    FlatButton(
+                      color: Color.fromARGB(255, 225, 223, 223),
+                      minWidth: 5,
+                      onPressed: () {},
+                      child: Text(
+                        "CUSTOM",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      splashColor: Colors.redAccent,
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 20, 20, 0),
+                    child: Text(
+                      "SEARCH BY NAME",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+              TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'First Name',
+                  ),
+                  textAlign: TextAlign.left),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 20, 20, 0),
+                    child: Text(
+                      "SEARCH BY GENDER",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  RaisedButton.icon(
+                    icon: Icon(
+                      Icons.male,
+                      color: Colors.black,
+                    ), // Your icon here
+                    label: Text(
+                      "MALE",
+                      style: TextStyle(color: Colors.black),
+                    ), // Your text here
+                    onPressed: () {},
+                  ),
+                  RaisedButton.icon(
+                    icon: Icon(
+                      Icons.female,
+                      color: Colors.orange,
+                    ), // Your icon here
+                    label: Text(
+                      "FEMALE",
+                      style: TextStyle(color: Colors.black),
+                    ), // Your text here
+                    onPressed: () {},
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "ALL",
+                      style: TextStyle(color: Colors.black),
+                    ), // Your text here
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 20, 20, 0),
+                    child: Text(
+                      "SORT BY",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: DropdownButton(
+                        isDense: true,
+                        iconEnabledColor: Color.fromARGB(255, 96, 8, 1),
+                        dropdownColor: Color.fromARGB(255, 225, 223, 223),
+                        value: selectedValue,
+                        items: [
+                          DropdownMenuItem(
+                            child: Text("None"),
+                            value: 0,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("FirstName"),
+                            value: 1,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("LastName"),
+                            value: 2,
+                          ),
+                          DropdownMenuItem(child: Text("Gender"), value: 3),
+                          DropdownMenuItem(
+                            child: Text("Date"),
+                            value: 4,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("DateTime"),
+                            value: 5,
+                          ),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            selectedValue = value;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      color: Color.fromARGB(255, 225, 223, 223),
+                      height: 38,
+                      width: 40,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.sort,
+                            size: 27,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 20,
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(mainAxisSize: MainAxisSize.max, children: [
+                    FlatButton(
+                      color: Colors.black,
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (_) => MyHomePage()),
+                            (Route<dynamic> route) => false);
+                      },
+                      child: Text(
+                        "FILTER",
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    )
+                  ]),
+                ),
+                height: 50,
+                color: Color.fromARGB(255, 225, 223, 223),
+              ),
+            ]),
+          ),
+        )));
   }
 }
--- login_signup/lib/Screens/HomePage.dart
+++ login_signup/lib/Screens/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_with_signup/Screens/Filter.dart';
+import 'package:login_with_signup/Screens/HomeForm.dart';
 import 'package:login_with_signup/Screens/LoginForm.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -25,24 +26,30 @@ class _MyHomePageState extends State<MyHomePage> {
           Row(
             children: [
               Icon(Icons.location_on, color: Color.fromARGB(255, 96, 8, 1)),
-              DropdownButton(
-                  value: selectedValue,
-                  items: [
-                    DropdownMenuItem(
-                      child: Text("CVR-AK-Anchorage"),
-                      value: 1,
-                    ),
-                    DropdownMenuItem(
-                      child: Text("CVR-AK-Fairbanks"),
-                      value: 2,
-                    ),
-                    DropdownMenuItem(child: Text("Others"), value: 3),
-                  ],
-                  onChanged: (value) {
-                    setState(() {
-                      selectedValue = value;
-                    });
-                  }),
+              DecoratedBox(
+                decoration: BoxDecoration(color: Colors.white),
+                child: DropdownButton(
+                    isDense: true,
+                    iconEnabledColor: Color.fromARGB(255, 96, 8, 1),
+                    dropdownColor: Color.fromARGB(255, 225, 223, 223),
+                    value: selectedValue,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("CVR-AK-Anchorage"),
+                        value: 1,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("CVR-AK-Fairbanks"),
+                        value: 2,
+                      ),
+                      DropdownMenuItem(child: Text("Others"), value: 3),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        selectedValue = value;
+                      });
+                    }),
+              ),
               Padding(padding: EdgeInsets.all(15)),
               Column(
                 children: [
@@ -63,38 +70,45 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                "September 2022",
-                style: TextStyle(fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  "September 2022",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            IconButton(
-              padding: EdgeInsets.only(left: 215),
-              icon: Icon(
-                Icons.restore,
-                color: Colors.black,
+              SizedBox(
+                width: 175,
               ),
-              onPressed: () => Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (_) => LoginForm()),
-                  (Route<dynamic> route) => false),
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.filter_list_alt,
-                color: Colors.black,
+              IconButton(
+                icon: Icon(
+                  Icons.restore,
+                  size: 25,
+                  color: Colors.black,
+                ),
+                onPressed: () => Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (_) => MyHomePage()),
+                    (Route<dynamic> route) => false),
               ),
-              onPressed: () => Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (_) => Filter()),
-                  (Route<dynamic> route) => false),
-            ),
-          ],
+              IconButton(
+                icon: Icon(
+                  Icons.filter_list_alt,
+                  color: Colors.black,
+                  size: 25,
+                ),
+                onPressed: () => Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (_) => Filter()),
+                    (Route<dynamic> route) => false),
+              ),
+            ],
+          ),
         ),
       ),
     );
